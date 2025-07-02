b536c240294f5649c26f6bab30a1c1fcae321169
SpiderTV2 <149539884+SpiderTV2@users.noreply.github.com>
2025-07-02 00:26:48 +0000

    Pending changes exported from your codespace

---
+++ bluetti-mqtt/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/with-contenv bashio
+
+# Starte bluetti-mqtt mit deinen Credentials
+bluetti-mqtt --cloud \
+    --username "info@cw-bauservice.de" \
+    --password "Th!mo001" \
+    --mqtt-host "mqtt://127.0.0.1"
